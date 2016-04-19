7489fd99a24a9c95efcd451a098c903ce33f661e
Cody Hinchliff <cody.hinchliff@gmail.com>
2016-04-19 14:25:28 -0700

    interface adjustments

--- ancestral-state-integrated/js/main.js
+++ ancestral-state-integrated/js/main.js
@@ -158,7 +158,7 @@ function getFlowAppByNameLookup(name) {
                             $.each($("#trait-table-vis").find("th"), function(i, headerCell) {
                                 var traitName = headerCell.text();
                                 var btn = $("div").addClass("btn btn-primary :hover").click(function() {
-                                    console.log("selected cell: " + traitName;
+                                    console.log("selected cell: " + traitName);
                                 });
                                 $(headerCell).html(btn);
                             });
