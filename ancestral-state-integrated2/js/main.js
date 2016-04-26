1dc433e580831791ffb1babcc1b8a589a6587c72
Cody Hinchliff <cody.hinchliff@gmail.com>
2016-04-25 18:14:20 -0700

    interface adjustments

--- ancestral-state-integrated2/js/main.js
+++ ancestral-state-integrated2/js/main.js
@@ -400,7 +400,7 @@ function renderTreePlot(target, tree, renderRequest, flow, girder, title=null, p
                         $("#send-trait-request").removeAttr("disabled");
                     } else {
                         setTimeout(_.bind(this.checkTraitResult, this), 1000);
-                        var msg = '<div class="spacer">Waiting for trait data... ' + traitDataCounter + '<br/><br/></div>';
+                        var msg = 'Waiting for trait data... ' + traitDataCounter;
 //                        console.log(msg);
                         $("#trait-loading-message").html(msg);
                         traitDataCounter++;
